5ef2f20335c7ed328cdaf96d2090752bda3f9a51
Cecep Solihin Yusup <cecepsolihinyusup@gmail.com>
2026-02-04 11:45:05 +0700

    feat: slicing login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Eye, EyeClosed } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -14,51 +22,26 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <input
+            type="text"
+            className="w-100 border-b-2"
+            placeholder="email" />
+          <div className="relative">
+            {
+              showPassword ?
+                <EyeClosed
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-0" /> :
+                <Eye
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-0" />
+            }
+            <input
+              type={showPassword ? "text" : "password"}
+              className="w-100 border-b-2"
+              placeholder="password" />
+          </div>
+          <button className="w-full bg-blue-400 text-white rounded">Submit</button>
         </div>
       </main>
     </div>
